6237dfea07758e97cf9784685ee1db1affee10a2
Vivek Goyal <vgoyal@redhat.com>
2018-08-29 11:15:58 -0400

    fuse: Add logic to free up a memory range
    Add logic to free up a busy memory range. Freed memory range will be
    returned to free pool. Add a worker which can be started to select
    and free some busy memory ranges.
    
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -714,6 +714,7 @@ void fuse_conn_init(struct fuse_conn *fc, struct user_namespace *user_ns,
 	fc->max_pages = FUSE_DEFAULT_MAX_PAGES_PER_REQ;
 	INIT_LIST_HEAD(&fc->free_ranges);
 	INIT_LIST_HEAD(&fc->busy_ranges);
+	INIT_DELAYED_WORK(&fc->dax_free_work, fuse_dax_free_mem_worker);
 }
 EXPORT_SYMBOL_GPL(fuse_conn_init);
 
@@ -722,6 +723,7 @@ void fuse_conn_put(struct fuse_conn *fc)
 	if (refcount_dec_and_test(&fc->count)) {
 		if (fc->destroy_req)
 			fuse_request_free(fc->destroy_req);
+		flush_delayed_work(&fc->dax_free_work);
 		if (fc->dax_dev)
 			fuse_free_dax_mem_ranges(&fc->free_ranges);
 		put_pid_ns(fc->pid_ns);
